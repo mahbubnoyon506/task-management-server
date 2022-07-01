fa5273d2ff276d477630d0abc6c08c48cd06722b
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-07-01 20:19:02 +0600

    Delete method added

--- index.js
+++ index.js
@@ -52,12 +52,12 @@ async function run(){
         res.send(result);
       })
 
-    //   app.delete('/tasks/:id', async(req, res) => {
-    //     const id = req.params.id;
-    //     const query = {_id: ObjectId(id)};
-    //     const result = await taskCollection.deleteOne(query);
-    //     res.send(result);
-    //   })
+     app.delete('/task/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await taskCollection.deleteOne(query)
+        res.send(result)
+     })
 
     app.post('/completetasks', async(req, res) => {
         const query = req.body;
